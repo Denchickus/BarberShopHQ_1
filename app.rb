41023516ec1450a2f0cbddcad0a6eba64bf6edc8
Denis Tarasov <denchickuz@yandex.ru>
2023-04-15 02:32:40 +0300

    Next commit

--- app.rb
+++ app.rb
@@ -4,7 +4,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :database, "sqlite3:barbershop.db" # подключение к БД
+# set :database, "sqlite3:barbershop.db" # подключение к БД
+
+set :database, {adapter: "sqlite3", database: "barbershop.db"} # подключение к БД
 
 class Client < ActiveRecord::Base # миграция - очередная версия нашей БД
 
