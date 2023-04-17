74f203dd895aa94b245a745bc6739666cc434790
Denis Tarasov <denchickuz@yandex.ru>
2023-04-17 16:08:16 +0300

    Add index.erb

--- app.rb
+++ app.rb
@@ -19,5 +19,7 @@ end
 
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index
 end
+
+# tux - консоль для ActiveRecord
